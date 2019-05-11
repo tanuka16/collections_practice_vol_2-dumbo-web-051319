be6094e295c733c2b71b42d8afb82f29faa4c63e
Tanuka Das <das.tanuka@yahoo.com>
2019-05-11 16:51:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,14 @@
 # your code goes here
 def begins_with_r(element)
-  element.all? {|item| item.start_with?("r")}
+  element.all? {|item| item.start_with?("r")}           #The all? method returns true if the block never returns false or nil
+                                                        #for any element passed to it
 end
 def contain_a(element)
   element.select{|item| item.include?('a')}
 end
 
 def first_wa(element)
-  element.find{|item| item.to_s.start_with?('wa')}
+  element.find{|item| item.to_s.start_with?('wa')}            #The find method returns the first element for which block is not false
 end
 
 def remove_non_strings(element)
@@ -22,7 +23,7 @@ def count_elements(element)
 end
 
 def merge_data(keys, data)
-  
+
 
 
 end
