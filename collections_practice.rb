4f4034e5f8e33884e58dbdd9dbf92cc0e42c6774
Tanuka Das <das.tanuka@yahoo.com>
2019-05-11 16:12:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
 # your code goes here
+def begins_with_r(element)
+  element.all? {|item| item.start_with?("r")}
+end
+def contain_a(element)
+  element.select{|item| item.include?('a')}
+end
+
+def first_wa(element)
+  element.find{|item| item.to_s.start_with?('wa')}
+end
+
+def remove_non_strings(element)
+  element.delete_if{|item| item != item.to_s}
+end
+
+def count_elements(element)
+  element.uniq.collect do |item|
+    item[:count] = element.count(item)
+    item
+  end
+end
+
+def merge_data(keys, data)
+  
+
+
+end
